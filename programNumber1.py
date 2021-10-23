b81e0db6a9cedf140a69972b991a6297af0c60f9
GezreelAzarcon <gezreelwazarcon@gmail.com>
2021-10-23 20:33:39 +0800

    A program that asks and displays the users name, age, and address.

---
+++ programNumber1.py
@@ -0,0 +1,7 @@
+#Create a program that will ask for name, age and address. 
+#Display those details in the following format.
+#Hi, my name is _____. I am ____ years old and I live in _____ .
+name = input('What is your name? ')
+age = input('What is your age? ')
+address = input('What is your address? ')
+print(f'Hi, my name is {name}. I am {age} years old and I live in {address}.')
